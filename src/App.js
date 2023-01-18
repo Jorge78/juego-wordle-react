18871828e59bc3811fbaeb548b6f7bcdd598f0e1
janr78 <jan.rock78@gmail.com>
2023-01-17 23:26:28 -0600

    Cambio de titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto React JS Juego Wordle
         </p>
         <a
           className="App-link"
